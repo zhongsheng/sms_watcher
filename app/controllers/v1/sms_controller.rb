7e8f3db97f2169e3f6d639dde028d0b6670d68e9
zhongsheng <nily.zhong@gmail.com>
2020-12-24 16:09:44 +0800

    fix

--- app/controllers/v1/sms_controller.rb
+++ app/controllers/v1/sms_controller.rb
@@ -1,19 +1,17 @@
+# frozen_string_literal: true
+
 class V1::SmsController < ActionController::API
   def callback
-    pp params[:token]
     raise 'Unauthrized!' if params[:token] != Digest::MD5.hexdigest(Rails.application.secret_key_base)
 
-    # binding.pry
     params['_json'].each do |data|
-    EntryLog.create!(
-      account_id: params[:account_id],
-      phone: data['phone_number'],
-      detail: data['err_msg']
-    )
+      EntryLog.create!(
+        account_id: params[:account_id],
+        phone: data['phone_number'],
+        detail: data['err_msg']
+      )
     end
 
-    pp EntryLog.all.size
-
-    render json: {code: 0, msg: '接收成功'}
+    render json: { code: 0, msg: '接收成功' }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  resources :accounts
+  resources :accounts do
+    resources :entry_logs
+  end
   namespace :v1 do
     post 'sms/callback/:account_id', to: 'sms#callback'
   end
-  resources :entry_logs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
